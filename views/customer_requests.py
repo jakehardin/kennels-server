80f9b3c0436b107855be7f70c3695331190c683f
jakehardin <jakehardin13@gmail.com>
2023-04-19 18:45:01 -0500

    test

---
+++ views/customer_requests.py
@@ -0,0 +1,28 @@
+CUSTOMERS = [
+    {
+        "id": 1,
+        "name": "Ryan Tanay"
+    }
+]
+
+def get_all_customers():
+    """DOCSTRING
+    """
+    return CUSTOMERS
+
+# Function with a single parameter
+def get_single_customer(id):
+    """DOCSTRING
+    """
+    # Variable to hold the found animal, if it exists
+    requested_customer = None
+
+    # Iterate the ANIMALS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for customer in CUSTOMERS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
